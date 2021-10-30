02094a1658c690b582698801c41d0fee9724df25
BlackKnightz <i_am_black_knight@outlook.com>
2021-10-29 23:00:30 -0300

    Excluindo

---
+++ www/js/firebase.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDyJ7Q0XGcm6P022w3C35KqDYlmsW6_Zdw",
+  authDomain: "cordova-1f10c.firebaseapp.com",
+  projectId: "cordova-1f10c",
+  storageBucket: "cordova-1f10c.appspot.com",
+  messagingSenderId: "16043472594",
+  appId: "1:16043472594:web:c5d98793c1e3862a495f0b",
+  measurementId: "G-TVJQ7ED8C9"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
